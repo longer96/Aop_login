a28431c84a4ace83dd2b2d0057ed740a743f9611
longer <longer>
2020-09-28 16:14:11 +0800

    通过AspectJ 实现 Aop切面

---
+++ app/src/main/java/com/longer/aop_login/OtherActivity.java
@@ -0,0 +1,20 @@
+package com.longer.aop_login;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OtherActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+
+
+}
+
+
